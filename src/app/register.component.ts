e97e158ccdadc84ed6cd9282becd2e38be483caa
fogham <s.fogham@hotmail.fr>
2019-05-22 20:50:54 +0200

    Validate with reactive forms

--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -1,26 +1,35 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   moduleId: module.id,
   selector: 'register',
   templateUrl:'register.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+        .error{
+            background-color: #fff0f0
+        }
+  `]
 })
 export class RegisterComponent {
     form;
 
     constructor(private fb:FormBuilder){
         this.form = fb.group({
-            firstName:'',
-            lastName:'',
-            email:'',
-            password:'',
-            confirmPassword:''
+            firstName: ['', Validators.required],
+            lastName:['', Validators.required],
+            email:['', Validators.required],
+            password:['', Validators.required],
+            confirmPassword:['', Validators.required]
         });
     }
 
     onSubmit(){
-        console.log(this.form.value);
+        console.log(this.form.valid);
+    }
+
+    isValid(control){
+       return this.form.controls[control].invalid && this.form.controls[control].touched;
     }
 }
