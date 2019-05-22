c26e63e9a5bf4f19ada901d69d5a538fba7789b7
fogham <s.fogham@hotmail.fr>
2019-05-22 21:08:22 +0200

    Validating passwords with a custom validator

--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -22,14 +22,22 @@ export class RegisterComponent {
             email:['', Validators.required],
             password:['', Validators.required],
             confirmPassword:['', Validators.required]
-        });
+        }, {validators: matchingFields('password','confirmPassword')});
     }
 
     onSubmit(){
-        console.log(this.form.valid);
+        console.log(this.form.errors);
     }
 
     isValid(control){
        return this.form.controls[control].invalid && this.form.controls[control].touched;
+    }
+}
+
+function matchingFields(field1, field2){
+    return form => {
+        if(form.controls[field1].value !== form.controls[field2].value){
+            return { matchingFields:true }
+        }
     }
 }
